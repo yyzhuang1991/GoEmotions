b8b4ae6bb2dbe16a4ec93e0280a262e61d0980f0
yyzhuang1991 <u1141153@utah.edu>
2023-07-31 16:08:57 -0600

    fix bug

--- run_goemotions.py
+++ run_goemotions.py
@@ -224,61 +224,61 @@ def main(cli_args):
     processor = GoEmotionsProcessor(args)
     label_list = processor.get_labels()
     print("haha")
-    config = BertConfig.from_pretrained(
-        args.model_name_or_path,
-        num_labels=len(label_list),
-        finetuning_task=args.task,
-        id2label={str(i): label for i, label in enumerate(label_list)},
-        label2id={label: i for i, label in enumerate(label_list)}
-    )
-    tokenizer = BertTokenizer.from_pretrained(
-        args.tokenizer_name_or_path,
-    )
-    model = BertForMultiLabelClassification.from_pretrained(
-        args.model_name_or_path,
-        config=config
-    )
+    # config = BertConfig.from_pretrained(
+    #     args.model_name_or_path,
+    #     num_labels=len(label_list),
+    #     finetuning_task=args.task,
+    #     id2label={str(i): label for i, label in enumerate(label_list)},
+    #     label2id={label: i for i, label in enumerate(label_list)}
+    # )
+    # tokenizer = BertTokenizer.from_pretrained(
+    #     args.tokenizer_name_or_path,
+    # )
+    # model = BertForMultiLabelClassification.from_pretrained(
+    #     args.model_name_or_path,
+    #     config=config
+    # )
 
-    # GPU or CPU
-    args.device = "cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu"
-    model.to(args.device)
+    # # GPU or CPU
+    # args.device = "cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu"
+    # model.to(args.device)
 
-    # Load dataset
-    train_dataset = load_and_cache_examples(args, tokenizer, mode="train") if args.train_file else None
-    dev_dataset = load_and_cache_examples(args, tokenizer, mode="dev") if args.dev_file else None
-    test_dataset = load_and_cache_examples(args, tokenizer, mode="test") if args.test_file else None
+    # # Load dataset
+    # train_dataset = load_and_cache_examples(args, tokenizer, mode="train") if args.train_file else None
+    # dev_dataset = load_and_cache_examples(args, tokenizer, mode="dev") if args.dev_file else None
+    # test_dataset = load_and_cache_examples(args, tokenizer, mode="test") if args.test_file else None
 
 
-    if dev_dataset is None:
-        args.evaluate_test_during_training = True  # If there is no dev dataset, only use test dataset
+    # if dev_dataset is None:
+    #     args.evaluate_test_during_training = True  # If there is no dev dataset, only use test dataset
 
-    if args.do_train:
-        global_step, tr_loss = train(args, model, tokenizer, train_dataset, dev_dataset, test_dataset)
-        logger.info(" global_step = {}, average loss = {}".format(global_step, tr_loss))
+    # if args.do_train:
+    #     global_step, tr_loss = train(args, model, tokenizer, train_dataset, dev_dataset, test_dataset)
+    #     logger.info(" global_step = {}, average loss = {}".format(global_step, tr_loss))
 
-    results = {}
-    if args.do_eval:
-        checkpoints = list(
-            os.path.dirname(c) for c in sorted(glob.glob(args.output_dir + "/**/" + "pytorch_model.bin", recursive=True))
-        )
-        if not args.eval_all_checkpoints:
-            checkpoints = checkpoints[-1:]
-        else:
-            logging.getLogger("transformers.configuration_utils").setLevel(logging.WARN)  # Reduce logging
-            logging.getLogger("transformers.modeling_utils").setLevel(logging.WARN)  # Reduce logging
-        logger.info("Evaluate the following checkpoints: %s", checkpoints)
-        for checkpoint in checkpoints:
-            global_step = checkpoint.split("-")[-1]
-            model = BertForMultiLabelClassification.from_pretrained(checkpoint)
-            model.to(args.device)
-            result = evaluate(args, model, test_dataset, mode="test", global_step=global_step)
-            result = dict((k + "_{}".format(global_step), v) for k, v in result.items())
-            results.update(result)
+    # results = {}
+    # if args.do_eval:
+    #     checkpoints = list(
+    #         os.path.dirname(c) for c in sorted(glob.glob(args.output_dir + "/**/" + "pytorch_model.bin", recursive=True))
+    #     )
+    #     if not args.eval_all_checkpoints:
+    #         checkpoints = checkpoints[-1:]
+    #     else:
+    #         logging.getLogger("transformers.configuration_utils").setLevel(logging.WARN)  # Reduce logging
+    #         logging.getLogger("transformers.modeling_utils").setLevel(logging.WARN)  # Reduce logging
+    #     logger.info("Evaluate the following checkpoints: %s", checkpoints)
+    #     for checkpoint in checkpoints:
+    #         global_step = checkpoint.split("-")[-1]
+    #         model = BertForMultiLabelClassification.from_pretrained(checkpoint)
+    #         model.to(args.device)
+    #         result = evaluate(args, model, test_dataset, mode="test", global_step=global_step)
+    #         result = dict((k + "_{}".format(global_step), v) for k, v in result.items())
+    #         results.update(result)
 
-        output_eval_file = os.path.join(args.output_dir, "eval_results.txt")
-        with open(output_eval_file, "w") as f_w:
-            for key in sorted(results.keys()):
-                f_w.write("{} = {}\n".format(key, str(results[key])))
+    #     output_eval_file = os.path.join(args.output_dir, "eval_results.txt")
+    #     with open(output_eval_file, "w") as f_w:
+    #         for key in sorted(results.keys()):
+    #             f_w.write("{} = {}\n".format(key, str(results[key])))
 
 
 if __name__ == '__main__':
