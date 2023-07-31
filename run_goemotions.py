366634e5aafbb4a7326bc1d6c909504a75d78eac
yyzhuang1991 <u1141153@utah.edu>
2023-07-31 16:07:29 -0600

    fix bug

--- run_goemotions.py
+++ run_goemotions.py
@@ -247,6 +247,7 @@ def main(cli_args):
     train_dataset = load_and_cache_examples(args, tokenizer, mode="train") if args.train_file else None
     dev_dataset = load_and_cache_examples(args, tokenizer, mode="dev") if args.dev_file else None
     test_dataset = load_and_cache_examples(args, tokenizer, mode="test") if args.test_file else None
+    print("haha")
 
     if dev_dataset is None:
         args.evaluate_test_during_training = True  # If there is no dev dataset, only use test dataset
